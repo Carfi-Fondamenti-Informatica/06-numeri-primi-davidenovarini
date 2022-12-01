9fbe6ef675c04d15ea1393be21239deac7cee2a8
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-01 12:37:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+
+int main() {
+int n ;
+cin>> n;
+bool risultato= funzione(n);
+if (risultato == true){
+    cout<<"numero primo"<<endl;
+}
+else
+{
+    cout<<"numero non primo"<<endl;
+}
+
+
+return 0;
 }
