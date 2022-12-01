710f910ca61d9fa9c4190aa71eb7cd5a1752fd8d
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-01 12:35:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include "lib.h"
+bool funzione (int n) {
+    int i=1,x;
 
+    if(n>1)
+        do{
+            i++;
+            x=n%i;
+        }while(x);
+    if(i==n){
+
+        return true;
+
+
+    }
+    else{
+        return false;
+    }
+
+}
